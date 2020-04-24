d4beaeea88e10c1f452c0f62b28a284bb1c823c1
Matthias Neumayer <neumayer.matt@gmail.com>
2020-04-24 11:20:45 +0200

    Part 2b: exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 
+const DisplayEntry = ({ person }) => (
+    <div key={person.name}> {person.name} {person.number} </div>
+)
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addEntry = (event) => {
     event.preventDefault()
@@ -14,28 +19,24 @@ const App = () => {
         return
     }
 
-    const newPerson = {name: newName}
+    const newPerson = {name: newName, number: newNumber}
     setPersons(persons.concat(newPerson))
   }
 
-  const handleNewNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleChangeNewName   = (event) => setNewName(event.target.value)
+  const handleChangeNewNumber = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addEntry}>
-        <div>
-          name: <input onChange={handleNewNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input onChange={handleChangeNewName} /></div>
+        <div>number: <input  onChange={handleChangeNewNumber} /></div>
+        <div><button type="submit">add</button></div>
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(p => <div key={p.name}> {p.name} </div>)}
+      {persons.map(p => <DisplayEntry person={p} /> )}
 
     </div>
   )
